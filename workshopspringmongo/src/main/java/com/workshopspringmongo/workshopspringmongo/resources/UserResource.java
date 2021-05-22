5841fc7dc4e6293b23b84b72f3f6395a51855f43
kaique oliveira santos <kaiquevicio@gmail.com>
2021-05-22 16:53:32 -0300

    getting a user by id

--- workshopspringmongo/src/main/java/com/workshopspringmongo/workshopspringmongo/resources/UserResource.java
+++ workshopspringmongo/src/main/java/com/workshopspringmongo/workshopspringmongo/resources/UserResource.java
@@ -5,6 +5,7 @@ import com.workshopspringmongo.workshopspringmongo.dto.UserDTO;
 import com.workshopspringmongo.workshopspringmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,5 +25,11 @@ public class UserResource {
         List<User> list = userService.findAll();
         List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDto);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        User user = userService.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(user));
     }
 }
--- workshopspringmongo/src/main/java/com/workshopspringmongo/workshopspringmongo/services/UserService.java
+++ workshopspringmongo/src/main/java/com/workshopspringmongo/workshopspringmongo/services/UserService.java
@@ -2,10 +2,13 @@ package com.workshopspringmongo.workshopspringmongo.services;
 
 import com.workshopspringmongo.workshopspringmongo.domain.User;
 import com.workshopspringmongo.workshopspringmongo.repository.UserRepository;
+import com.workshopspringmongo.workshopspringmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -14,5 +17,10 @@ public class UserService {
 
     public List<User> findAll() {
         return userRepository.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> user = userRepository.findById(id);
+        return user.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
     }
 }
