51a78a277083aa4d4753614ce64caf70a3783552
kaique oliveira santos <kaiquevicio@gmail.com>
2021-05-22 13:07:18 -0300

    Project created

---
+++ workshopspringmongo/src/test/java/com/workshopspringmongo/workshopspringmongo/WorkshopspringmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshopspringmongo.workshopspringmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopspringmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
