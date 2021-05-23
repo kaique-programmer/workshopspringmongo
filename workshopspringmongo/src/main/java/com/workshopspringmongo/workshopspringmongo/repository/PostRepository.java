bc5e15c24bbcddfadd326d36ed8f37483777c128
kaique oliveira santos <kaiquevicio@gmail.com>
2021-05-23 16:56:11 -0300

    Querying data with multiple criteria

--- workshopspringmongo/src/main/java/com/workshopspringmongo/workshopspringmongo/repository/PostRepository.java
+++ workshopspringmongo/src/main/java/com/workshopspringmongo/workshopspringmongo/repository/PostRepository.java
@@ -4,12 +4,16 @@ import com.workshopspringmongo.workshopspringmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
+import java.util.Date;
 import java.util.List;
 
 public interface PostRepository extends MongoRepository<Post, String> {
 
-    @Query("{'title': { $regex: ?0, $options: 'i' } }")
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
     List<Post> searchTitle(String text);
 
     List<Post> findByTitleContainingIgnoreCase(String text);
+
+    @Query("{ $and: [{ date: {$gte: ?1 } }, { date: { $lte: ?2 } } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+    List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
--- workshopspringmongo/src/main/java/com/workshopspringmongo/workshopspringmongo/resources/util/URL.java
+++ workshopspringmongo/src/main/java/com/workshopspringmongo/workshopspringmongo/resources/util/URL.java
@@ -2,6 +2,10 @@ package com.workshopspringmongo.workshopspringmongo.resources.util;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class URL {
 
@@ -10,6 +14,16 @@ public class URL {
             return URLDecoder.decode(text, "UTF-8");
         } catch (UnsupportedEncodingException e) {
             return "";
+        }
+    }
+
+    public static Date convertDate(String textDate, Date defaultValue) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        simpleDateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
+        try {
+            return simpleDateFormat.parse(textDate);
+        } catch (ParseException e) {
+            return defaultValue;
         }
     }
 }
